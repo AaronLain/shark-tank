c24ac44b8091dfaa348e1817023ee2b43bd3c858
Aaron Lain <aaron.j.lain@gmail.com>
2020-02-29 10:16:35 -0600

    jquery > vanilla dom (printToDom 2.0)

--- javascripts/helpers/utils.js
+++ javascripts/helpers/utils.js
@@ -1,6 +1,5 @@
-const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
+const printToDom = (divId, textToPrint) => { 
+    $(`#${divId}`).html(textToPrint);
 }
 
 export default { printToDom }
