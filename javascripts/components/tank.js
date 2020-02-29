1f2dfb8f0011ccfe19b4a7f68d08a900f1445f4c
Aaron Lain <aaron.j.lain@gmail.com>
2020-02-29 10:31:24 -0600

    list of names works

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -1,7 +1,17 @@
 import util from '../helpers/utils.js';
+import personData from '../helpers/data/personData.js'
 
 const tankBuilder = () => {
-    let domString = 'yo'
+    const persons = personData.getAlivePersons();
+    let domString = ''
+    domString += `<div class="card">`
+    domString += `<div class="card-header">SHARK TANK</div>`
+    domString += `<ul class="list-group list-group-flush">`
+    persons.forEach((person) => {
+        domString += `<li class="list-group-item">${person.name}</li>`
+    });
+    domString += `</ul>`
+    domString += `</div>`
     util.printToDom('tank', domString);
 }
 
