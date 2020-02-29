78a4be8c367e924feee848d3a892fd96beb4fd60
Aaron Lain <aaron.j.lain@gmail.com>
2020-02-29 09:55:24 -0600

    added data

---
+++ styles/javascripts/helpers/data/personData.js
@@ -0,0 +1,157 @@
+const persons = [
+    {
+        id: 'person1',
+        name: 'zoe',
+        isDead: false
+    },
+
+    {
+        id: 'person2',
+        name: 'luke',
+        isDead: false
+    },
+
+    {
+        id: 'person3',
+        name: 'mary',
+        isDead: false
+    },
+
+    {
+        id: 'person4',
+        name: 'aaron',
+        isDead: false
+    },
+
+    {
+        id: 'person5',
+        name: 'anca',
+        isDead: false
+    },
+
+    {
+        id: 'person6',
+        name: 'beth',
+        isDead: false
+    },
+
+    {
+        id: 'person7',
+        name: 'bethany',
+        isDead: false
+    },
+
+    {
+        id: 'person8',
+        name: 'chris',
+        isDead: false
+    },
+
+    {
+        id: 'person9',
+        name: 'david',
+        isDead: false
+    },
+
+    {
+        id: 'person10',
+        name: 'davis',
+        isDead: false
+    },
+
+    {
+        id: 'person11',
+        name: 'dylan',
+        isDead: false
+    },
+
+    {
+        id: 'person12',
+        name: 'joey',
+        isDead: false
+    },
+    {
+        id: 'person13',
+        name: 'john',
+        isDead: false
+    },
+
+    {
+        id: 'person14',
+        name: 'jose',
+        isDead: false
+    },
+
+    {
+        id: 'person15',
+        name: 'kayla',
+        isDead: false
+    },
+
+    {
+        id: 'person16',
+        name: 'kenneth',
+        isDead: false
+    },
+
+    {
+        id: 'person17',
+        name: 'liza',
+        isDead: false
+    },
+
+    {
+        id: 'person18',
+        name: 'michele',
+        isDead: false
+    },
+
+    {
+        id: 'person19',
+        name: 'monique',
+        isDead: false
+    },
+
+    {
+        id: 'person20',
+        name: 'ola',
+        isDead: false
+    },
+
+    {
+        id: 'person21',
+        name: 'sarah',
+        isDead: false
+    },
+
+    {
+        id: 'person22',
+        name: 'stephen',
+        isDead: false
+    },
+
+    {
+        id: 'person23',
+        name: 'steven',
+        isDead: false
+    },
+
+    {
+        id: 'person24',
+        name: 'todd',
+        isDead: false
+    },
+
+    {
+        id: 'person25',
+        name: 'zac',
+        isDead: true
+    }
+
+];
+
+const getAlivePersons = () => {
+    return persons.filter((x) => x.isDead === false);
+};
+
+export default { getAlivePersons }
